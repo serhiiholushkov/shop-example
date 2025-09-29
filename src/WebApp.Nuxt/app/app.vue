3ce0cf1caecd8fffdd966260f3bcbb3a144231a1
Serhii Holushkov <serhii.holushkov@gmail.com>
2025-09-28 21:37:17 -0400

    Nuxt i18n added

--- src/WebApp.Nuxt/app/app.vue
+++ src/WebApp.Nuxt/app/app.vue
@@ -2,5 +2,16 @@
   <div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
+    <div>
+      <span>{{ $t('welcome') }}</span>
+    </div>
+    <div>
+      <span>a second attempt with script func:</span>
+      <span>{{ t('welcome') }}</span>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+const { t } = useI18n();
+</script>
--- src/WebApp.Nuxt/nuxt.config.ts
+++ src/WebApp.Nuxt/nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', 'plugin:prettier/recommended', 'prettier'],
+  modules: ['@nuxt/eslint', '@nuxtjs/i18n'],
+  i18n: {
+    locales: [{ code: 'en', name: 'English', file: 'en.json' }],
+    defaultLocale: 'en',
+    langDir: 'locales/',
+  },
 });
